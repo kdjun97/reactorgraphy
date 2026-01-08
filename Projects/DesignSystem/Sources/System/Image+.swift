55bad691ff77ff6538749a56bc905ccb338a51be
kdjun97 <kdjun97@gmail.com>
2026-01-08 23:20:06 +0900

    feat/19: Image resize를 위한 extension 추가

--- Projects/DesignSystem/Sources/System/Image+.swift
+++ Projects/DesignSystem/Sources/System/Image+.swift
@@ -5,4 +5,14 @@
 //  Created by 김동준 on 1/2/26
 //
 
+import UIKit
+
 public typealias RImages = DesignSystemAsset.Images
+
+public extension UIImage {
+    func resized(to size: CGSize) -> UIImage {
+        UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
